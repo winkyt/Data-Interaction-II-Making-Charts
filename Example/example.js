5bcc88278db919fdc7c4b1ddd4cadacd59e23b4c
winkyt <winkyton@hotmail.com>
2017-05-08 18:00:41 -0700

    Add files via upload

--- Example/example.js
+++ Example/example.js
@@ -1,53 +1,41 @@
-/*The original code is from www.packtpub.com and modified by Yingqi Tang*/
+/**
+ * Created by yingqitang on 5/7/17.
+ */
+/* Hard Code your information */
+var frequency_list = [{"text":"study","size":40},{"text":"motion","size":15},{"text":"forces","size":10},{"text":"electricity","size":15},{"text":"movement","size":10},{"text":"relation","size":5},{"text":"things","size":10},{"text":"force","size":5},{"text":"ad","size":5},{"text":"energy","size":85},{"text":"living","size":5},{"text":"nonliving","size":5},{"text":"laws","size":15},{"text":"speed","size":45},{"text":"velocity","size":30},{"text":"define","size":5},{"text":"constraints","size":5},{"text":"universe","size":10},{"text":"physics","size":120},{"text":"describing","size":5},{"text":"matter","size":90},{"text":"physics-the","size":5},{"text":"world","size":10},{"text":"works","size":10},{"text":"science","size":70},{"text":"interactions","size":30},{"text":"studies","size":5},{"text":"properties","size":45},{"text":"nature","size":40},{"text":"branch","size":30},{"text":"concerned","size":25},{"text":"source","size":40},{"text":"google","size":10},{"text":"defintions","size":5},{"text":"two","size":15},{"text":"grouped","size":15},{"text":"traditional","size":15},{"text":"fields","size":15},{"text":"acoustics","size":15},{"text":"optics","size":15},{"text":"mechanics","size":20},{"text":"thermodynamics","size":15},{"text":"electromagnetism","size":15},{"text":"modern","size":15},{"text":"extensions","size":15},{"text":"thefreedictionary","size":15},{"text":"interaction","size":15},{"text":"org","size":25},{"text":"answers","size":5},{"text":"natural","size":15},{"text":"objects","size":5},{"text":"treats","size":10},{"text":"acting","size":5},{"text":"department","size":5},{"text":"gravitation","size":5},{"text":"heat","size":10},{"text":"light","size":10},{"text":"magnetism","size":10},{"text":"modify","size":5},{"text":"general","size":10},{"text":"bodies","size":5},{"text":"philosophy","size":5},{"text":"brainyquote","size":5},{"text":"words","size":5},{"text":"ph","size":5},{"text":"html","size":5},{"text":"lrl","size":5},{"text":"zgzmeylfwuy","size":5},{"text":"subject","size":5},{"text":"distinguished","size":5},{"text":"chemistry","size":5},{"text":"biology","size":5},{"text":"includes","size":5},{"text":"radiation","size":5},{"text":"sound","size":5},{"text":"structure","size":5},{"text":"atoms","size":5},{"text":"including","size":10},{"text":"atomic","size":10},{"text":"nuclear","size":10},{"text":"cryogenics","size":10},{"text":"solid-state","size":10},{"text":"particle","size":10},{"text":"plasma","size":10},{"text":"deals","size":5},{"text":"merriam-webster","size":5},{"text":"dictionary","size":10},{"text":"analysis","size":5},{"text":"conducted","size":5},{"text":"order","size":5},{"text":"understand","size":5},{"text":"behaves","size":5},{"text":"en","size":5},{"text":"wikipedia","size":5},{"text":"wiki","size":5},{"text":"physics-","size":5},{"text":"physical","size":5},{"text":"behaviour","size":5},{"text":"collinsdictionary","size":5},{"text":"english","size":5},{"text":"time","size":35},{"text":"distance","size":35},{"text":"wheels","size":5},{"text":"revelations","size":5},{"text":"minute","size":5},{"text":"acceleration","size":20},{"text":"torque","size":5},{"text":"wheel","size":5},{"text":"rotations","size":5},{"text":"resistance","size":5},{"text":"momentum","size":5},{"text":"measure","size":10},{"text":"direction","size":10},{"text":"car","size":5},{"text":"add","size":5},{"text":"traveled","size":5},{"text":"weight","size":5},{"text":"electrical","size":5},{"text":"power","size":5}];
 
-var width = 750,
-    height = 500;
+/* set layout  */
+var width =750, height =350
 
-var fill = d3.scale.category20();
+var color = d3.scale.linear()
+    .domain([0,1,2,3,4,5,6,10,15,20,100])
+    .range(['#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#f7f7f7','#d1e5f0','#92c5de','#4393c3','#2166ac','#053061']);
 
-d3.csv('Example.csv', function (data) {
-    var state = [];
-    data.forEach(function(row){
-        if (row.G > 0) state.push({text: row.Name, size: Number(row.G)});
-    });
+d3.layout.cloud().size([width, height])/* pre-determine width*/
+    .words(frequency_list)
+    .padding(6)
+    .rotate(function() { return ~~(Math.random() * 2) * 90; }) /*No rotation, rotate(0)*/
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw)
+    .start();
 
-    var state = state.sort(function(a,b){
-        return (a.size < b.size)? 1:(a.size == b.size)? 0:-1
-    }).slice(0,100);
-
-    var stateScale = d3.scale.linear()
-        .range([10,60])
-        .domain([d3.min(state,function(d) { return d.size; }),
-            d3.max(state,function(d) { return d.size; })
-        ]);
-
-    d3.layout.cloud().size([width, height])
-        .words(state)
-        .padding(0)
-        //      .rotate(function() { return ~~(Math.random() * 2) * 90; })
-        .font("Impact")
-        .fontSize(function(d) { return stateScale(d.size); })
-        .on("end", drawCloud)
-        .start();
-});
-
-
-function drawCloud(words) {
-    d3.select("#word-cloud").append("svg")
+function draw(words) {
+    d3.select("#word-cloud").append("svg") /*change from body to #word-cloud)*/
         .attr("width", width)
-        .attr("height", height)
+        .attr("height",height)
+        .attr("class", "wordcloud")
         .append("g")
+        // without the transform, words words would get cutoff to the left and top, they would
+        // appear outside of the SVG area
         .attr("transform", "translate("+(width / 2)+","+(height / 2)+")")
         .selectAll("text")
         .data(words)
         .enter().append("text")
         .style("font-size", function(d) { return d.size + "px"; })
-        .style("font-family", "Impact")
-        .style("fill", function(d, i) { return fill(i); })
-        .attr("text-anchor", "middle")
+        .style("fill", function(d, i) { return color(i); })
+        .attr("text-anchor","middle")
         .attr("transform", function(d) {
             return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
         })
         .text(function(d) { return d.text; });
 }
-
